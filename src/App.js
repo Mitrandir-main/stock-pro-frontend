4024c3806e49fb2ffdf2af28dfc5ae904da4be62
Mitrandir-main <dian.kumanov@yahoo.com>
2023-04-14 15:32:16 +0300

    [add] first chart working

--- src/App.js
+++ src/App.js
@@ -3,45 +3,17 @@ import './App.css';
 import RecipeReviewCard from './components/home/Card';
 import HomePage from './components/home/HomePage';
 import ResponsiveAppBar from './components/navigation/ResponsiveAppBar';
+import { Grid } from '@mui/material';
+import ChartLine from './components/home/Chart';
 
 function App() {
-  const [arrayData, setArrayData] = useState([]);
-
-  useEffect(() => {
-    //get data
-    setArrayData([
-    "Page 1", 
-    "Page ",
-    "Page 4",
-    "Page 4",
-    "Page 4",
-    "Page 44",
-    "Page 44",
-    "Page 44",
-    "Page 44",
-    "Page 44",
-    "Page 14",
-    "Page 44",
-    "Page 44",
-    "Page 44",
-
-   ])
-  }, [])
 
   return (
     <div className="App">
       <ResponsiveAppBar/>
-      <header className="App-header">
-        <div>
-          {arrayData.map((x)=> {
-            return (
-             <div>
-                <HomePage name = {x}/>
-                <RecipeReviewCard name = {x}/>
-             </div>)
-          })}
-        </div>
-      </header>
+      <Grid>
+      <ChartLine/>
+      </Grid>
     </div>
   );
 }
