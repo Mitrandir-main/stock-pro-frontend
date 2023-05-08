5dcd25e63d26b68f162da83fb90a3079dc0c1e68
Mitrandir-main <dian.kumanov@yahoo.com>
2023-05-08 16:18:50 +0300

    [add] clean up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import RecipeReviewCard from './components/home/Card';
-import HomePage from './components/home/HomePage';
 import ResponsiveAppBar from './components/navigation/ResponsiveAppBar';
-import { Grid } from '@mui/material';
-import ChartLine from './components/home/Chart';
-import Chart2 from './components/home/Chart2';
-import ChartRenderer from './components/home/ChartRenderer';
 import { WebSocketDemo } from './components/home/SocketConnection';
 
 function App() {
